b58833f2b4f76e2ff955b479d9ef8cfd14f26d2f
Leandro Coelho <lecoelhoofficial@gmail.com>
2021-08-02 23:40:59 -0300

    Atualizando a view Produtos

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,7 @@ const SiteController = require('../controllers/SiteController');
 router.get('/', SiteController.index);
 router.get('/produto', SiteController.produto);
 router.get('/cadastroproduto', SiteController.cadastroproduto);
+router.get('/login', SiteController.login);
+router.get('/cadastro', SiteController.cadastro);
+router.get('/perfil', SiteController.perfil);
 module.exports = router;
